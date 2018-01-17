4b3563e74672e9ca4d59a767343c798fa00eb9d1
yushangqun <yushangqun@hotmail.com>
2018-01-17 11:07:34 +0800

    separate data from the code

--- main.py
+++ main.py
@@ -16,41 +16,80 @@ from kivy.config import Config
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.text import LabelBase
+from kivy.properties import ObjectProperty
+from kivy.uix.button import Button
+from kivy.uix.modalview import ModalView
+from kivy.storage.jsonstore import JsonStore
 
+#import some fonts
 LabelBase.register(name="PTSans", fn_regular="font/PTC55F.ttf")
 LabelBase.register(name="PTSansBold", fn_regular="font/PTS76F.ttf")
 LabelBase.register(name="PTSansThin", fn_regular="font/PTS55F.ttf")
 
+#load Json File
 
-class CustomFloatLayout(FloatLayout):
+store = JsonStore('test.json')
+
+
+
+class ProductPopup(ModalView):
+    pass
+
+class MainView(FloatLayout):
 
     def __init__(self, **kwargs):
-        super(CustomFloatLayout, self).__init__(**kwargs)
+        super(MainView, self).__init__(**kwargs)
+
+        for x in range (0, store.count()):
+            tmpProduct = ProductThumbNailWidget()
+            tmpProductId = 'product' + str(x)
+            tmpProduct.id = tmpProductId
+            tmpProduct.ids.productName.text = store.get(tmpProductId)['name']
+            tmpProduct.ids.productImage.source = store.get(tmpProductId)['productImageSource']
+            self.ids.product_Display.add_widget(tmpProduct)
 
 
-    def callback(self):
-        Factory.ProductPopup().open()
+        self.ids.product_Display.size_hint_x=0.25 * store.count()
+
     def callback2(self):
+        print("helloworld")
         Factory.TrailerPopup().open()
     def scrollDown(self):
         self.ids["frontScrollView"].scroll_to( self.ids["myVideoPlayer"])
 
 
-#function for ThumbNailWidget. 
-class ProductThumbNailWidget(Widget):
-    def on_touch_down(self, touch):
-        if self.collide_point(*touch.pos):
-            Factory.ProductPopup().open()
+# function for ThumbNailWidget.
+class ProductThumbNailWidget(Button):
+    def click(button):
 
+        # clear the color for the previous button
+        for child in button.parent.children:
+            child.background_color = (1,1,1,1)
+
+        # identfy the selected button by put on a different color
+        button.background_color = (0,160,66,.9)
+
+        # create a new popup and set context accordingly
+        theProductPopup= ProductPopup()
+        theProductPopup.ids.productDetailText.text = store.get(button.id)['productDetail']
+        theProductPopup.ids.productName.text = store.get(button.id)['name']
+        theProductPopup.ids.productImage.source = store.get(button.id)['productImageSource']
+        theProductPopup.open()
+
+
+
+class ProductDisplayGridLayout(GridLayout):
+    pass
 
 
 class homeEntertaimentApp(App):
 
     def build(self):
-        #from the sake of proper displaying on my laptop, needed to delete for mobile device
+        # for the sake of proper displaying on my laptop, needed to delete for mobile device
         Window.size = (960, 540)
 
-        return CustomFloatLayout()
+        return MainView()
+
 
 
 
